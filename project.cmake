8d6ae94693a8fbec98276e0d2c1eeabe597fa2c5
thomaschoi143 <thomasa143a@gmail.com>
2023-07-08 14:25:44 +1000

    Fix duplicate in project.cmake

--- project.cmake
+++ project.cmake
@@ -1,13 +1,12 @@
 # This CMake file is intended to register project-wide objects so they can be
 # reused easily between deployments, but also by other projects.
 
-add_fprime_subdirectory("${CMAKE_CURRENT_LIST_DIR}/Components/Controllers/Adapter/")
-add_fprime_subdirectory("${CMAKE_CURRENT_LIST_DIR}/Components/Controllers/PacketSender/")
-
 add_fprime_subdirectory("${CMAKE_CURRENT_LIST_DIR}/Components/Structs/SubSystemEnum/")
 add_fprime_subdirectory("${CMAKE_CURRENT_LIST_DIR}/Components/Structs/DataPacket/")
 add_fprime_subdirectory("${CMAKE_CURRENT_LIST_DIR}/Components/Structs/AdapterPorts/")
-add_fprime_subdirectory("${CMAKE_CURRENT_LIST_DIR}/Components/Behaviors/ScienceMode/")
-add_fprime_subdirectory("${CMAKE_CURRENT_LIST_DIR}/Components/Structs/AdapterPorts/")
-add_fprime_subdirectory("${CMAKE_CURRENT_LIST_DIR}/Components/Behaviors/ScienceMode/")
+
 add_fprime_subdirectory("${CMAKE_CURRENT_LIST_DIR}/Components/Controllers/CamController/")
+add_fprime_subdirectory("${CMAKE_CURRENT_LIST_DIR}/Components/Controllers/Adapter/")
+add_fprime_subdirectory("${CMAKE_CURRENT_LIST_DIR}/Components/Controllers/PacketSender/")
+
+add_fprime_subdirectory("${CMAKE_CURRENT_LIST_DIR}/Components/Behaviors/ScienceMode/")
